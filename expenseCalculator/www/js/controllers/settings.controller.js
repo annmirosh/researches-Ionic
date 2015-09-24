5eb06c88c154c6d177a6a33a2cb64253a5599dc4
annmirosh <mac@Ann-2.local>
2015-09-24 17:04:29 +0500

    Remove button was added to list of Products on Settings view

--- expenseCalculator/www/js/controllers/settings.controller.js
+++ expenseCalculator/www/js/controllers/settings.controller.js
@@ -13,19 +13,23 @@
     vm.settings = {
       dailyLimit: DataKeeperService.dailyLimit,
       defaultProducts: DataKeeperService.favoriteProducts
-    }
+    };
 
     vm.save = save;
     vm.addProduct = addProduct;
+    vm.removeDefaultProduct = removeDefaultProduct;
 
     function save() {
-      debugger;
       DataKeeperService.save('dailyLimit', vm.settings.dailyLimit);
     }
 
     function addProduct() {
       vm.settings.defaultProducts.push({name: vm.settings.product});
       DataKeeperService.save('favoriteProducts', vm.settings.defaultProducts);
+    }
+
+    function removeDefaultProduct(index) {
+      vm.settings.defaultProducts.splice(index, 1);
     }
   }
 })();
